159761a71ec2470c0f708001d567e458e273aec1
Mark Ma <markworldamc@gmail.com>
2024-12-05 11:00:35 -0500

    a

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,18 @@ import sqlite3, urllib.request, urllib.parse, json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def map_page():
     geodb_key = open("keys/key_geodb.txt", "r").read().strip().rstrip()
     if len(geodb_key) == 0:
         return "YOU MUST ADD YOUR API KEY IN keys/key_geodb.txt !!!"
-    
+    print(request.method)
     return render_template("index.html")
+
+@app.route('/sus', methods=['POST'])
+def sus():
+    print(request.form.get('data'))
+    return redirect('/')
 
 if __name__ == "__main__":
     app.debug = True
