483b2d673699dea86e337c151c3d2389c6d46741
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-12-05 23:58:42 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,17 @@ P01: Topher Time
 2024-03-12
 Time Spent: 3 hours
 """
+
+from flask import Flask, render_template, url_for, session, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/calendar", methods=['GET', 'POST'])
+def calendar_page():
+    return render_template('calendar.html')
+
+@app.route("/weather", methods=['GET', 'POST'])
+def weather_page():
+    return render_template('weather.html')
+
+    
